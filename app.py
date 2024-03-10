2315da7cff288b4209bb8d5cbfad5d0a7bb40bcd
Dang Nguyen Hai <59546270+dangblack@users.noreply.github.com>
2024-03-10 18:29:01 +0700

    Factorial app v1
    Factorial Cal

---
+++ app.py
@@ -0,0 +1,13 @@
+import streamlit as st
+from factorial import fact
+
+def main():
+    st.title("Factorial Cal")
+    number = st.number_input("Enter a number: ", min_value=0,max_value=100)
+
+    if st.button("Calculate"):
+        result = fact(number)
+        st.write(f"The factorial of {number} is {result}")
+        st.balloons()
+if __name__ == "__main__":
+    main()
